2c65e41bb684415ace9965f16183e61e0d9949e8
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-07 13:59:41 -0600

    new

--- src/test/java/hello/HelloWorld.java
+++ src/test/java/hello/HelloWorld.java
@@ -19,10 +19,21 @@ public class HelloWorld {
         System.out.println(x);
 
 
-        int x = 4;
-        x += 5;
+//        int x = 4;
+//        x += 5;
+//
+//        System.out.println(x);
 
-        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y += x;
+//
+//        System.out.println(y);
+
+
+
+
 
 
 
