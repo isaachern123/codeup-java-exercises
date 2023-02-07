b3bf92d956aefd7e0a12ec9c88df58743bde5b14
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-07 13:53:37 -0600

    new

--- src/test/java/hello/HelloWorld.java
+++ src/test/java/hello/HelloWorld.java
@@ -2,7 +2,30 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        //program starts executing here
-        System.out.println("Hello World");
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+
+
+        String myString = "Hello World";
+        System.out.println(myString);
+
+
+        float myNumber = 3.14F;
+
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+
+        int x = 4;
+        x += 5;
+
+        System.out.println(x);
+
+
+
+
     }
 }
