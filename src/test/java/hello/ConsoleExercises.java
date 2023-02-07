3d19da2dd353283ce3e4ae1d00db0dd2cdaacdf7
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-07 15:06:12 -0600

    Practice

---
+++ src/test/java/hello/ConsoleExercises.java
@@ -0,0 +1,26 @@
+package hello;
+
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String aName = "name";
+        aName = input.nextLine();
+        System.out.println("Your name is " + aName);
+
+        System.out.println("Enter your age: ");
+        int age = input.nextInt();
+        System.out.println("Your age is " + age);
+
+        input.nextLine();
+        System.out.println("Enter your name");
+        aName = input.nextLine();
+        System.out.println("Your name is " + aName);
+
+        input.close();
+
+    }
+}
