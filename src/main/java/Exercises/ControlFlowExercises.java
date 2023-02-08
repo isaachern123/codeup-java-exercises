41d48b119a1fd69fdbeabd9fe6378a56b8b22df8
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-08 11:15:39 -0600

    2 loops

---
+++ src/main/java/Exercises/ControlFlowExercises.java
@@ -0,0 +1,24 @@
+package Exercises;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+
+//        for(int i = 5; i < 16; i++) {
+//            System.out.println(i);
+//            if (i == 15) {
+//                break;
+//            }
+//        }
+
+//        for (int i = 2; i < 1000000; i*= i) {
+//            System.out.println(i);
+//            if (i == 65536) {
+//                break;
+//            }
+//        }
+
+
+
+    }
+}
