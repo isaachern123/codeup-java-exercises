e8ca6d34f165edc672265c2b08004caa9e16b26c
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-08 15:12:44 -0600

    finished

--- src/main/java/Exercises/ControlFlowExercises.java
+++ src/main/java/Exercises/ControlFlowExercises.java
@@ -33,41 +33,33 @@ public class ControlFlowExercises {
 //            }
 //        }
 
-//        int number = input.nextInt();
+//            System.out.println("How many rows?");
+//            int number = input.nextInt();
+//            System.out.printf("%-7s| %-8s| %-5s%n", "number", "squared", "cubed");
 //
-//
-//
-//        System.out.printf("      Here is your table         %n");
-//
-//
-//        System.out.printf("--------------------------------%n");
-//        System.out.printf("| %1s | %1s | %1s |%n", "NUMBER", "SQUARED", "CUBED");
-//        System.out.printf("--------------------------------%n");
-//
-//        System.out.printf("| %01d | %01d | %01d |%n", number, "double",  64);
-//        System.out.printf("| %01d | %01d | %01d |%n", "Floating", "float",   32);
-//        System.out.printf("| %01d | %01d | %01d |%n", "Integral", "long",    64);
-//        System.out.printf("| %01d | %01d | %01d |%n", "Integral", "int",     32);
-//
-//
-//        System.out.printf("--------------------------------%n");
+//            for (int i = 1; i <= number; i++) {
+//                long squared = (long) i * i;
+//                long cubed = (long) i * i * i;
+//                System.out.printf("%-7s| %-8s| %-5s%n", i, squared, cubed);
+//            }
 
-        System.out.println("What is your grade?");
-        int grade = input.nextInt();
 
-        if ((grade <= 100) && (grade >= 88)) {
-            System.out.println("A");
-        } else if ((grade <= 87) && (grade >= 80)) {
-            System.out.println("B");
-        } else if ((grade <= 79) && (grade >= 67)) {
-            System.out.println("C");
-        } else if ((grade <= 66) && (grade >= 60)) {
-            System.out.println("D");
-        } else if ((grade <= 59) && (grade >= 0)) {
-            System.out.println("F");
-        } else {
-            System.out.println("Not Possible");
-        }
+//        System.out.println("What is your grade?");
+//        int grade = input.nextInt();
+//
+//        if ((grade <= 100) && (grade >= 88)) {
+//            System.out.println("A");
+//        } else if ((grade <= 87) && (grade >= 80)) {
+//            System.out.println("B");
+//        } else if ((grade <= 79) && (grade >= 67)) {
+//            System.out.println("C");
+//        } else if ((grade <= 66) && (grade >= 60)) {
+//            System.out.println("D");
+//        } else if ((grade <= 59) && (grade >= 0)) {
+//            System.out.println("F");
+//        } else {
+//            System.out.println("Not Possible");
+//        }
 
 
         input.close();
