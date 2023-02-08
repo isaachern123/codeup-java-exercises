9798b2e0d35fa569ecdbb14cab097db8ff721bc1
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-08 11:22:12 -0600

    fizzBuzz

--- src/main/java/Exercises/ControlFlowExercises.java
+++ src/main/java/Exercises/ControlFlowExercises.java
@@ -18,6 +18,18 @@ public class ControlFlowExercises {
 //            }
 //        }
 
+        for (int i = 0; i < 100; i++) {
+            if ( (i % 3 == 0) && (i % 5 == 0) ) {
+                System.out.println("fizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
 
 
     }
