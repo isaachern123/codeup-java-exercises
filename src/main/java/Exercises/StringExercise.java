aa05be1317156494527bf7ff025132950ed13b8a
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-09 09:54:25 -0600

    strings

---
+++ src/main/java/Exercises/StringExercise.java
@@ -0,0 +1,33 @@
+package Exercises;
+
+import java.sql.SQLOutput;
+
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String first = """
+                We don't need no education
+                We don't need no thought control
+                """;
+
+        System.out.println(first);
+
+
+        String second = "Check \"this\" out!, \"s inside of \"s! \n";
+
+        System.out.println(second);
+
+
+        String third = "In windows, the main drive is usually C: \n";
+
+        System.out.println(third);
+
+        String fourth = """
+                I can do backslashes \\, double backslashes \\\\,
+                and the amazing triple backslash \\\\\\!
+                """;
+        System.out.println(fourth);
+
+
+    }
+}
