6982b91bf678e1a89ffc14b76f1542c2b3eab388
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-10 12:13:12 -0600

    simple math methods

--- src/main/java/Exercises/MethodsExercise.java
+++ src/main/java/Exercises/MethodsExercise.java
@@ -1,4 +1,26 @@
 package Exercises;
 
 public class MethodsExercise {
+    public static double addition(double n1, double n2) {
+        return n1 + n2;
+    }
+    public static double subtraction(double n1, double n2) {
+        return n1 - n2;
+    }
+    public static double multiplication(double n1, double n2) {
+        return n1 * n2;
+    }
+    public static double division(double n1, double n2) {
+        return n1 / n2;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(addition(1, 2));
+        System.out.println(subtraction(1, 2));
+        System.out.println(multiplication(1, 2));
+        System.out.println(division(1, 2));
+
+    }
+
 }
