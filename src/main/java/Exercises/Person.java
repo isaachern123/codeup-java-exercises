8ef0d9b2d3d7d48ad1ef936e0c90697a62fe8538
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-13 11:49:50 -0600

    private

--- src/main/java/Exercises/Person.java
+++ src/main/java/Exercises/Person.java
@@ -6,7 +6,7 @@ public class Person {
 
     private String lastName;
 
-    public String sayHello() {
+    private String sayHello() {
         return String.format("Hello from %s %s!", firstName, lastName);
     }
 
