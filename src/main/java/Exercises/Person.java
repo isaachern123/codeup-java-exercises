2cbdbddbdb58f300dd5bc0a24ec17769bfebe66a
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-13 12:29:38 -0600

    getting started

--- src/main/java/Exercises/Person.java
+++ src/main/java/Exercises/Person.java
@@ -2,43 +2,85 @@ package Exercises;
 
 public class Person {
 
-    private String firstName;
+    private String name;
 
-    private String lastName;
 
-    public String sayHello() {
-        return String.format("Hello from %s %s!", firstName, lastName);
+
+    public void sayHello(){
+        System.out.println(name);
     }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    //accessors
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
 
     public static void main(String[] args) {
-        Person rick = new Person();
-        rick.firstName = "Rick";
-        rick.lastName = "Sanchez";
-        System.out.println(rick.sayHello());
 
-        Person sue = new Person();
-        sue.firstName = "Sue";
-        sue.lastName = "Smith";
-        System.out.println(sue.sayHello());
     }
-
-    // accessors
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+//
+//    private String firstName;
+//
+//    private String lastName;
+//    private String middlename;
+//
+//
+//    public Person(String firstName, String lastName, String middlename) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.middlename = middlename;
+//    }
+//
+//    public String sayHello() {
+//        return String.format("Hello from %s %s!", firstName, lastName);
+//    }
+//
+//    public Person() {
+//        firstName = "";
+//        lastName = "";
+//        middlename = "";
+//    }
+//
+//    public static void main(String[] args) {
+//        Person rick = new Person();
+//        rick.firstName = "Rick";
+//        rick.lastName = "Sanchez";
+//        System.out.println(rick.sayHello());
+//
+//        Person sue = new Person();
+//        sue.firstName = "Sue";
+//        sue.lastName = "Smith";
+//        System.out.println(sue.sayHello());
+//    }
+//
+//    // accessors
+//
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
 }
 
