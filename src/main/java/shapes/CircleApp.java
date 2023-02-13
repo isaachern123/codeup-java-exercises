fac17bc5f411c63e28df4b769f7dc20a65cf7af9
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-13 15:18:38 -0600

    CircleAPP

---
+++ src/main/java/shapes/CircleApp.java
@@ -0,0 +1,15 @@
+package shapes;
+
+
+import java.util.Scanner;
+
+public class CircleApp {
+
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("enter radius for circle");
+
+    }
+
+}
