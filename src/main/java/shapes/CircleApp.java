d2ace1278fb23da835ecedb0f0cab296219019f6
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-13 15:32:29 -0600

    almost finished circle

--- src/main/java/shapes/CircleApp.java
+++ src/main/java/shapes/CircleApp.java
@@ -3,13 +3,21 @@ package shapes;
 
 import java.util.Scanner;
 
+import static util.Input.yesNo;
+
 public class CircleApp {
 
-    Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("enter radius for circle");
-
+        double newRadius = input.nextDouble();
+        Circle c1 = new Circle(newRadius);
+        System.out.println("this is your area " + c1.getArea());
+        System.out.println("this is your circumference " + c1.getCircumference());
+        System.out.println("do you want to make another circle? yes or no?");
+        String yesNo = input.nextLine();
+        yesNo(yesNo);
     }
 
 }
