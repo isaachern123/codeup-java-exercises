fbf4473a5182491f4c38bed36e29796bb080a0a1
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-14 14:02:24 -0600

    finished server name generator

--- src/main/java/Exercises/ServerNameGenerator.java
+++ src/main/java/Exercises/ServerNameGenerator.java
@@ -5,8 +5,8 @@ public class ServerNameGenerator {
     static String[] nouns = {"person", "place", "thing", "man", "woman", "home", "office", "town", "table", "car"};
 
     public static String getRandomString(String[] arr){
-        int min = 0; // Minimum value of range
-        int max = 10;
+        int min = -1; // Minimum value of range
+        int max = 9;
 
         int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
         return arr[random_int];
@@ -14,8 +14,11 @@ public class ServerNameGenerator {
     }
 
     public static void main(String[] args) {
-        System.out.println(getRandomString(adjectives));
-        System.out.println(getRandomString(nouns));
+//        System.out.println(getRandomString(adjectives));
+//        System.out.println(getRandomString(nouns));
+
+        System.out.println("Here is your server name: ");
+        System.out.println(getRandomString(adjectives) + "-" + getRandomString(nouns));
     }
 
 
