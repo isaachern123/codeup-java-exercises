19608f0b4f7373f01a21f15edfce90efce404719
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-14 12:23:27 -0600

    add person method

--- src/main/java/Exercises/ArraysExercises.java
+++ src/main/java/Exercises/ArraysExercises.java
@@ -4,16 +4,44 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
+    public static String[] addX(int stringLength, String[] oldArr, String newName)
+    {
+        int i;
+
+        // create a new array of size n+1
+        String[] newarr = new String[stringLength + 1];
+
+        // insert the elements from
+        // the old array into the new array
+        // insert all elements till n
+        // then insert x at n+1
+        for (i = 0; i < stringLength; i++)
+            newarr[i] = oldArr[i];
+
+        newarr[stringLength] = newName;
+
+        return newarr;
+    }
+
 
     public static void main(String[] args) {
 
-        String [] Person = new String[3];
+        int stringLength = 3;
+
+        String [] Person = new String[stringLength];
+
 
         Person[0] = "Isaac";
         Person[1] = "Serina";
         Person[2] = "Charlie";
         System.out.println(Arrays.toString(Person));
 
+
+        String[] newArr = addX(stringLength, Person, "ruben");
+        System.out.println(Arrays.toString(newArr));
+
+        String[] newerArr = addX(4,newArr,"Jacob");
+        System.out.println(Arrays.toString(newerArr));
 
     }
 }
