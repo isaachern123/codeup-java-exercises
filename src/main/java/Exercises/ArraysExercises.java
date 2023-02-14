51db3487e5cea93044ee86f63b6388ff1d119dc2
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-14 11:29:23 -0600

    getting started with the arrays

---
+++ src/main/java/Exercises/ArraysExercises.java
@@ -0,0 +1,19 @@
+package Exercises;
+
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+
+    public static void main(String[] args) {
+
+        String [] Person = new String[3];
+
+        Person[0] = "Isaac";
+        Person[1] = "Serina";
+        Person[2] = "Charlie";
+        System.out.println(Arrays.toString(Person));
+
+
+    }
+}
