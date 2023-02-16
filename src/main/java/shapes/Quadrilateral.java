177ffcdad93623619f80d1fdff862db1160e25c4
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-16 16:14:18 -0600

    cleared

--- src/main/java/shapes/Quadrilateral.java
+++ src/main/java/shapes/Quadrilateral.java
@@ -3,14 +3,31 @@ package shapes;
 abstract class Quadrilateral extends Shape
     implements Measurable {
 
+    protected int length;
 
-    @Override
-    public double getPerimeter() {
-        return 0;
+    protected int width;
+
+
+    public Quadrilateral(int length, int width) {
+        this.length = length;
+        this.width = width;
     }
 
-    @Override
-    public double getArea() {
-        return 0;
+
+
+
+    //accessors
+
+    public int getLength() {
+        return length;
     }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public abstract void setLength(int length);
+    public abstract void setWidth(int width);
+
+}
 }
