5fa108afa7a0ad3dc0956b9223477767be2917a5
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-16 16:18:12 -0600

    done

--- src/main/java/shapes/Square.java
+++ src/main/java/shapes/Square.java
@@ -1,4 +1,31 @@
 package shapes;
 
-public class Square extends Quadrilateral {
+public class Square extends Quadrilateral
+    implements Measurable {
+
+    protected int side;
+
+    public Square(int length, int width) {
+        super(length, width);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * length + 2 * width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public void setLength(int length) {
+        this.side = length;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.side = width;
+    }
 }
