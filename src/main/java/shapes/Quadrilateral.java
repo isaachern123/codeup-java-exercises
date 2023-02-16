1ac7f40fa5680864bfe55fdb5b52eeaf2fbfc2a8
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-16 13:36:42 -0600

    cleared

--- src/main/java/shapes/Quadrilateral.java
+++ src/main/java/shapes/Quadrilateral.java
@@ -1,4 +1,16 @@
 package shapes;
 
-abstract class Quadrilateral extends Shape {
+abstract class Quadrilateral extends Shape
+    implements Measurable {
+
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
 }
