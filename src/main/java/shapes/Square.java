db8301de7f312d2af046759454f025d75fd98bef
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-16 11:38:32 -0600

    cleared

--- src/main/java/shapes/Square.java
+++ src/main/java/shapes/Square.java
@@ -2,28 +2,3 @@ package shapes;
 
 public class Square extends Rectangle{
 
-    private double side;
-
-    public Square(double side) {
-        super(side, side);
-        this.side = side;
-    }
-
-//    public Square(double length, double width) {
-//        super(length, width);
-//    }
-
-
-
-    @Override
-    public double getArea() {
-        System.out.println("square");
-        return side * side;
-    }
-
-    @Override
-    public double getPerimeter() {
-        System.out.println("square");
-        return side * 4;
-    }
-}
