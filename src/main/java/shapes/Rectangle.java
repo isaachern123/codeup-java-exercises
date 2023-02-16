e5549be328af3a8c0449679eb007c6e93257f62a
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-16 16:14:24 -0600

    cleared

--- src/main/java/shapes/Rectangle.java
+++ src/main/java/shapes/Rectangle.java
@@ -1,5 +1,26 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral
+    implements Measurable {
 
+
+    @Override
+    public double getPerimeter() {
+        return 2 * length + 2 * width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+    }
 }
