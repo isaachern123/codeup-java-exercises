e14fa45ce790cb6e3947583da812e85fc8aef874
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-16 11:38:36 -0600

    cleared

--- src/main/java/shapes/ShapesTest.java
+++ src/main/java/shapes/ShapesTest.java
@@ -3,13 +3,7 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5, 4);
-        Rectangle box2 = new Square(5);
-        System.out.println("area: " + box1.getArea());
-        System.out.println("perimeter: " + box1.getPerimeter());
-        System.out.println("area: " + box2.getArea());
-        System.out.println("perimeter: " + box2.getPerimeter());
-        System.out.println("all done");
+
 
 
 
