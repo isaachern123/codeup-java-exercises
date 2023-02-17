130e4604a285a1482f3f3ef7fdffd6d0138008a3
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-17 09:38:02 -0600

    stuff

--- src/main/java/exercises/Lecture.java
+++ src/main/java/exercises/Lecture.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Lecture {
+public class LectureHashMap {
     public static void main(String[] args) {
 
 
