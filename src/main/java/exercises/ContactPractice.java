8e8cc845e3233c1636ee326641f56b3d5c0e94bd
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-22 10:51:20 -0600

    project practice

--- src/main/java/exercises/ContactPractice.java
+++ src/main/java/exercises/ContactPractice.java
@@ -1,4 +1,38 @@
 package exercises;
 
 public class ContactPractice {
+    public String name;
+
+    public int phoneNumber;
+
+    public ContactPractice(String name, int phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactPractice{" +
+                "name='" + name + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+
+    //accessors
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
