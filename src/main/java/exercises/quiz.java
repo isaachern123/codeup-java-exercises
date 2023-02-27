24600d5ff4643b201f88b2b2912eae81daedd44d
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-27 14:37:05 -0600

    StringUtils Apache Commons

--- src/main/java/exercises/quiz.java
+++ src/main/java/exercises/quiz.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class quiz {
 
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(13);
-        numbers.add(new Integer(8));
-        numbers.add(3.6);
+//        List<Integer> numbers = new ArrayList<>();
+//        numbers.add(13);
+//        numbers.add(new Integer(8));
+//        numbers.add(3.6);
 
     }
 }
