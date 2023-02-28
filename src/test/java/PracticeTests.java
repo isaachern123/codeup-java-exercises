2590ac88bbbca5fa4ef678b3fd619978ad1095d8
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-28 10:07:33 -0600

    Testing Java

---
+++ src/test/java/PracticeTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PracticeTests {
+
+    @Test
+    public void testAlwaysTrue() {
+        assertTrue(true);
+    }
+}
